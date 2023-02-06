870073b8ea8beb4831fe611bf4c8bd2ee5f51691
Dekrazi <122207627+Dekrazi@users.noreply.github.com>
2023-02-06 18:58:42 +0100

    test

--- test_main.py
+++ test_main.py
@@ -1,4 +1,19 @@
-# import unittest
-# import controller
+import unittest
+from controller import add_task
+
+import unittest
+
+class TestAddTask(unittest.TestCase):
+    def test_add_task(self):
+        taskList = []
+        add_task(taskList)
+
+        self.assertEqual(len(taskList), 1)
+        self.assertEqual(taskList[0].title, "TestT")
+        self.assertEqual(taskList[0].desc, "TestD")
+    
+        
+if __name__ == '__main__':
+    unittest.main()
 
 
