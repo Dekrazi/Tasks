321cf4e4d5d9b966272cdc4ef8418ac683a86be3
Dekrazi <122207627+Dekrazi@users.noreply.github.com>
2023-02-07 17:48:48 +0100

    playing with tests

--- test_main.py
+++ test_main.py
@@ -1,18 +1,29 @@
 import unittest
-from controller import add_task
+from controller import add_task, del_task
 
 import unittest
 
-class TestAddTask(unittest.TestCase):
-    def test_add_task(self):
-        taskList = []
-        add_task(taskList)
+class TestTasks(unittest.TestCase):
+    # def test_add_task(self):
+    #     taskList = []
+    #     add_task(taskList)
 
-        self.assertEqual(len(taskList), 1)
-        self.assertEqual(taskList[0].title, "TestT")
-        self.assertEqual(taskList[0].desc, "TestD")
+    #     self.assertEqual(len(taskList), 1)
+    #     self.assertEqual(taskList[0].title, "TestT")
+    #     self.assertEqual(taskList[0].desc, "TestD")
     
-        
+    def test_del_task(self):
+        taskList = [
+            {"title": "Cleaning", "desc": "Vacuuming"},
+            {"title": "Sports", "desc": "Running"},
+            {"title": "Homework", "desc": "Math"}
+        ]
+
+        result = del_task(taskList)
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result, [{"title": "Cleaning", "desc": "Vacuuming"}, 
+                                {"title": "Sports", "desc": "Running"}])
+
 if __name__ == '__main__':
     unittest.main()
 
