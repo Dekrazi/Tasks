9041b6c54e05db9787824f472212fb24b0e3c5dd
Dekrazi <122207627+Dekrazi@users.noreply.github.com>
2023-02-07 18:03:21 +0100

    playing with tests

--- test_main.py
+++ test_main.py
@@ -1,28 +1,29 @@
 import unittest
-from controller import add_task, del_task
+from model import Task
+from controller import add_task, list_tasks
 
 import unittest
 
 class TestTasks(unittest.TestCase):
-    # def test_add_task(self):
-    #     taskList = []
-    #     add_task(taskList)
+    def test_add_task(self):
+        taskList = []
+        add_task(taskList)
 
-    #     self.assertEqual(len(taskList), 1)
-    #     self.assertEqual(taskList[0].title, "TestT")
-    #     self.assertEqual(taskList[0].desc, "TestD")
+        self.assertEqual(len(taskList), 1)
+        self.assertEqual(taskList[0].title, "TestT")
+        self.assertEqual(taskList[0].desc, "TestD")
     
-    def test_del_task(self):
-        taskList = [
-            {"title": "Cleaning", "desc": "Vacuuming"},
-            {"title": "Sports", "desc": "Running"},
-            {"title": "Homework", "desc": "Math"}
-        ]
+    def test_list_tasks(self):
+        taskList = []
+        list_tasks(taskList)
+        taskList.append(Task('task1', 'desc1'))
+        taskList.append(Task('task2', 'desc2'))
 
-        result = del_task(taskList)
-        self.assertEqual(len(result), 2)
-        self.assertEqual(result, [{"title": "Cleaning", "desc": "Vacuuming"}, 
-                                {"title": "Sports", "desc": "Running"}])
+        self.assertEqual(len(taskList), 2)
+        self.assertEqual(taskList[0].title, "task1")
+        self.assertEqual(taskList[0].desc, "desc1")
+        self.assertEqual(taskList[1].title, "task2")
+        self.assertEqual(taskList[1].desc, "desc2")
 
 if __name__ == '__main__':
     unittest.main()
