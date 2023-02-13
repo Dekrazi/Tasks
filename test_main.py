a6dc6a99bf344e8688b8b9b484d213fe865b655f
Dekrazi <122207627+Dekrazi@users.noreply.github.com>
2023-02-13 12:37:09 +0100

    New branch (#8)
    * more tests testing
    
    
    * working tests
    
    * working tests
    
    * squash test
    
    * TESTS WORKING
    
    * Revert "merge"
    
    * Revert "Revert "merge""
    
    This reverts commit 761a81257e60eeb0f78f758503aab38d4bae3ffe.

--- test_main.py
+++ test_main.py
@@ -1,18 +1,37 @@
 import unittest
 from model import Task
-from controller import add_task, list_tasks
+from controller import add_task, list_tasks, update_task, del_task, del_task
 
 import unittest
 
 class TestTasks(unittest.TestCase):
+    
+        
     def test_add_task(self):
         taskList = []
         add_task(taskList)
 
         self.assertEqual(len(taskList), 1)
-        self.assertEqual(taskList[0].title, "TestT")
-        self.assertEqual(taskList[0].desc, "TestD")
-    
+        self.assertEqual(taskList[0].title, "new title")
+        self.assertEqual(taskList[0].desc, "new desc")
+
+    def test_update_task_by_index(self):
+        task_list = [Task("Clean the House", "Wipe down counters"),
+                    Task("Grocery Store", "Buy bread")]
+        #Assuming user wants to update task with index[0]
+        updated_task_list = update_task(task_list)
+        self.assertEqual(updated_task_list[0].title, "updated title")
+        self.assertEqual(updated_task_list[0].desc, "updated desc")
+
+    def test_update_task_by_title(self):
+        task_list = [Task("Clean the House", "Wipe down counters"),
+                    Task("Grocery Store", "Buy bread")]
+        #Assuming user wants to update task with title Grocery Store
+        task_list = update_task(task_list)
+        self.assertEqual(task_list[1].title, "updated title")
+        self.assertEqual(task_list[1].desc, "updated desc")
+        
+
     def test_list_tasks(self):
         taskList = []
         list_tasks(taskList)
@@ -25,7 +44,25 @@ class TestTasks(unittest.TestCase):
         self.assertEqual(taskList[1].title, "task2")
         self.assertEqual(taskList[1].desc, "desc2")
 
+    def test_del_task(self):  
+        taskList = []
+
+        task1 = Task("Clean the House", "Wipe down counters")
+        taskList.append(task1)
+        task2 = Task("Go to the Grocery Store", "Buy bread")
+        taskList.append(task2)
+        #Assuming user wants to delete task with index[0]
+        taskList = del_task(taskList)
+        self.assertEqual(len(taskList), 1)
+        self.assertEqual(taskList[0].title, "Go to the Grocery Store")
+        self.assertEqual(taskList[0].desc, "Buy bread")
+
+            
+        
+
 if __name__ == '__main__':
     unittest.main()
+    
+        
 
 
